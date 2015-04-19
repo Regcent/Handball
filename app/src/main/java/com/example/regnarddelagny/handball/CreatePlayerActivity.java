fc83e95a98ac20e25beeacf966ee2cf64bdf6b41
Regcent <vincent.regnard.de.lagny@gmail.com>
2015-04-19 23:39:40 +0200

    Initial commit

--- app/src/main/java/com/example/regnarddelagny/handball/CreatePlayerActivity.java
+++ app/src/main/java/com/example/regnarddelagny/handball/CreatePlayerActivity.java
@@ -3,6 +3,9 @@ package com.example.regnarddelagny.handball;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class CreatePlayerActivity extends Activity {
@@ -13,5 +16,12 @@ public class CreatePlayerActivity extends Activity {
         setContentView(R.layout.activity_create_player);
     }
 
-
+    public void createNewPlayer(View view) {
+        EditText nom = (EditText) findViewById(R.id.playerName);
+        EditText prenom = (EditText) findViewById(R.id.playerFirstName);
+        EditText numero = (EditText) findViewById(R.id.playerNumber);
+        Log.d("Nom joueur",nom.getText().toString());
+        Log.d("Prenom joueur", prenom.getText().toString());
+        Log.d("Numero joueur", numero.getText().toString());
+    }
 }
